31a03d6a60f169a54724d543dbeb8ef1e64617e0
Kingsley Etoka <etokakingsley@gmail.com>
2022-10-23 06:45:35 +0100

    Added app themeing

--- app/src/main/java/co/innovatespace/worldnews/NewsApp.kt
+++ app/src/main/java/co/innovatespace/worldnews/NewsApp.kt
@@ -1,8 +1,31 @@
 package co.innovatespace.worldnews
 
 import android.app.Application
+import co.innovatespace.data.preference.PrefStoreManager
+import co.innovatespace.ui.ThemeHelper
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltAndroidApp
 class NewsApp: Application() {
+    @Inject
+    lateinit var prefStoreManager: PrefStoreManager
+
+    override fun onCreate() {
+        super.onCreate()
+        initialize()
+    }
+
+    private fun initialize() = GlobalScope.launch(Dispatchers.Main) {
+        // Todo("This should be refactored to be passed through the domain layer")
+        prefStoreManager.themeMode.flowOn(Dispatchers.IO).collect{
+            ThemeHelper.applyTheme( it )
+
+        }
+
+    }
 }
---
+++ common/ui/src/main/java/co/innovatespace/ui/ThemeHelper.kt
@@ -0,0 +1,24 @@
+package co.innovatespace.ui
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatDelegate
+
+ object ThemeHelper {
+    fun applyTheme (theme: Int){
+        when(theme){
+            AppCompatDelegate.MODE_NIGHT_NO -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+            AppCompatDelegate.MODE_NIGHT_YES -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+            else -> {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY)
+                }
+            }
+        }
+    }
+}
