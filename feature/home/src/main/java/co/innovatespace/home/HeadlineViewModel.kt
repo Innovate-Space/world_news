0b205e46fb1760736449b3b2cfe046c6de9ff5a6
Kingsley Etoka <etokakingsley@gmail.com>
2022-10-28 20:56:19 +0100

    viewmodel update

--- feature/home/src/main/java/co/innovatespace/home/HeadlineViewModel.kt
+++ feature/home/src/main/java/co/innovatespace/home/HeadlineViewModel.kt
@@ -1,13 +1,19 @@
 package co.innovatespace.home
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.paging.PagingData
 import co.innovatespace.home.usecases.GetNews
+import co.innovatespace.ui.presentation.UINews
 import co.innovatespace.utility.DispatchersProvider
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 @HiltViewModel
-class HeadlineViewModel @Inject constructor(private val getNews: GetNews, private val dispatchProvider: DispatchersProvider, ) : ViewModel() {
+class HeadlineViewModel @Inject constructor(private val getNews: GetNews, private val dispatchProvider: DispatchersProvider, private val savedStateHandle: SavedStateHandle) : ViewModel() {
+
+    lateinit var pagingDataFlow : Flow<PagingData<UINews>>
     init {
         println("I executed")
         val result = getNews()
