f447eb5f4f0f002db6c70e00f388061fc50bb7be
Kingsley Etoka <etokakingsley@gmail.com>
2022-11-03 22:17:33 +0100

    created layout for loading error recycler indicator

---
+++ common/ui/src/main/java/co/innovatespace/ui/NewsLoadingAdapter.kt
@@ -0,0 +1,9 @@
+package co.innovatespace.ui
+
+import androidx.paging.LoadStateAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+//class NewsLoadingAdapter: LoadStateAdapter<> {
+//
+//    inner class LoadingStateViewHolder(): RecyclerView.ViewHolder()
+//}
